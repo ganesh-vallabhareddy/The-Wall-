fc9f3857267182e1e04308e0e2319bad2753ea42
vallabhareddy-ganesh <vallabhareddyganesh@gmail.com>
2023-07-02 18:22:17 +0530

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:wall/firebase_options.dart';
+import 'package:wall/pages/home_page.dart';
 
 import 'auth/auth.dart';
 
